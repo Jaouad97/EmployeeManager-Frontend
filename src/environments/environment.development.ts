4076f976925111ba93589da9657d8bf0c3dcff76
Jaouad Aboudaoud <tjbao97@gmx.de>
2024-01-02 16:38:34 +0100

    Project

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,11 +1,4 @@
-/*
-export const environment = {
-    production: true
-  };
-
-  */
-
-  export const environment = {
+ export const environment = {
     production: false,
     apiBaseUrl: 'http://localhost:8080'
 };
